3d5367375f1d7854590c2ce15e3294ebb45c4760
Gitea <julien.lejoly2712@gmail.com>
2019-06-27 21:01:31 +0200

    Adding eula support + part of algo

---
+++ install.sh
@@ -0,0 +1,10 @@
+# Installing Dependencies
+sudo apt install -y python3.7
+sudo apt install -y git
+sudo apt install -y openjdk-8-jre
+
+# Copy the content
+sudo mkdir /opt/MCSS
+sudo cp * /opt/MCSS
+
+# Add
---
+++ main.py
@@ -0,0 +1,16 @@
+# Get backup of precedent installation
+
+# Ask the MC version
+# Get MC version
+# Run server.jar
+# Run cp /opt/MCSS/eula.txt .
+# Run server.jar
+# Write stop to server.jar
+
+# Add a new server.properties files as append
+# Write a new line with #Minecraft server properties
+# Write a new line with #(File Modification Datestamp)
+# Write a new allow-nether=true
+# Write a new line with enforce-whitelist=false
+
+# Ask 
